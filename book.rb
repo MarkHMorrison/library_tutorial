31428319a55bae0f32348c47f15ddbda58e02507
Jason York <github@yorkspace.com>
2012-11-19 22:22:11 -0600

    Adding features for Dojo session 2

--- book.rb
+++ book.rb
@@ -1,15 +1,19 @@
 class Book
-	attr_accessor :title, :author, :category, :status
+	attr_accessor :title, :author, :category, :status, :due_on, :patrons_waiting, :fine
 	def initialize title, author, category
 		@title = title
 		@author = author
 		@category = category
 		@status = :available
+		@due_on = nil
+		@patrons_waiting = 0
+		@fine = 0
 	end
 
 	def checkout
 		if @status == :available 
 			@status = :on_loan
+			@due_on = Date.today + 14
 		else
 			raise 'Book is already checked out'
 		end
@@ -17,7 +21,16 @@ class Book
 
 	def checkin
 		if @status == :on_loan
-			@status = :available
+			if patrons_waiting?
+				@patrons_waiting -= 1
+				@status = :on_hold
+			else
+				@status = :available
+			end
+
+			if @due_on < Date.today
+				@fine = Date.today - @due_on
+			end
 		else
 			raise 'Book is not checked out'
 		end
@@ -29,5 +42,29 @@ class Book
 
 	def available?
 		@status == :available
+	end
+
+	def hold
+		if @status == :available
+			@status = :on_hold
+		elsif @status == :on_loan
+			@patrons_waiting += 1
+		end
+	end
+
+	def on_hold?
+		@status == :on_hold
+	end
+
+	def patrons_waiting?
+		@patrons_waiting > 0
+	end
+
+	def has_fine?
+		@fine > 0
+	end
+
+	def overdue?
+		@due_on < Date.today
 	end
 end
--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -27,38 +27,98 @@ describe Book do
 		end
 	end
 
-	describe "#checkout" do
-		context "when book is available" do
+	describe "checking out a book" do
+		before :each do
+			@book.checkout
+		end
+
+		context "when it is available" do
 			it "changes status to loaned" do
-				@book.status.should eql :available
-				@book.checkout
 				@book.status.should eql :on_loan
 			end
 		end
+		it "is due 14 weeks from now" do
+			@book.due_on.should == Date.today + 14
+		end
+
 		context "when book is not available" do
 			it "raises an error" do
-				@book.checkout
-				@book.status.should eql :on_loan
 				lambda { @book.checkout }.should raise_error
 			end
 		end
 	end
 
-	describe "#checkin" do
+	describe "checking in a book" do
 		context "when book is on loan" do
-			it "changes status to available" do
+			before :each do
 				@book.checkout
-				@book.status.should eql :on_loan
 				@book.checkin
+			end
+			it "changes status to available" do
 				@book.status.should eql :available
 			end
 		end
 		context "when book is available" do
+			before :each do
+				@book.checkout
+				@book.checkin
+			end
 			it "raises an error" do
-				@book.status.should eql :available	
 				lambda { @book.checkin }.should raise_error
 			end
 		end
+		context "when book has waiting patrons" do
+			before :each do
+				@book.checkout
+				@book.hold
+				@book.checkin
+			end
+			it { @book.should be_on_hold }
+			it "should decrement the waiting list" do
+				@book.patrons_waiting.should == 0
+			end
+		end
+		context "when book is overdue" do
+			before :each do
+				@book.checkout
+				@book.due_on = Date.today - 5
+				@book.checkin
+			end
+			it { @book.should be_overdue }
+
+			it "should have a fine" do
+				@book.should have_fine
+			end
+			it "should have a fine amount of $1 per day" do
+				@book.fine.should == 5
+			end
+		end
+	end
+
+	describe "placing a book on hold" do
+		context "when book is available" do
+			before :each do
+				@book.hold
+			end
+
+			it { @book.should be_on_hold }
+
+			it "should not increment the waiting list" do
+				@book.patrons_waiting.should == 0
+			end
+		end
+
+		context "when book is on loan" do
+			before :each do
+				@book.checkout
+				@book.hold
+			end
+
+			it "should increment the waiting list" do
+				@book.patrons_waiting.should == 1
+			end
+		end
+
 	end
 	
 	describe "#lost" do
